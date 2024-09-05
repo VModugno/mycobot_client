d125bfbde98670178fff87383b68edd09e00fda4
MZandtheRaspberryPi <ziegltrum@gmail.com>
2024-09-05 18:51:18 +0100

    adding mycobot client

--- mycobot_client_2/setup.py
+++ mycobot_client_2/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'cobot_client = mycobot_interface_2.cobot_client:main'
+            'cobot_client = mycobot_client_2.cobot_client:main'
         ],
     },
 )
