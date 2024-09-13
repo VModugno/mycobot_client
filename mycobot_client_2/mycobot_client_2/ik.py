8a5ef7f47bdc3995a3339ff5c767b8b28401dd1b
MZandtheRaspberryPi <ziegltrum@gmail.com>
2024-09-13 15:53:49 +0100

    updating ik to listen to poses

--- mycobot_client_2/mycobot_client_2/ik.py
+++ mycobot_client_2/mycobot_client_2/ik.py
@@ -1,4 +1,10 @@
+import rclpy
+from rclpy.node import Node
+
 from mycobot_client_2.cobot_client import MycobotClient, CurAngles
+from mycobot_msgs_2.msg import (MycobotSetAngles,
+                                MycobotPose,
+                                MycobotGripperStatus)
 
 import numpy as np
 import time
@@ -9,72 +15,96 @@ from simulation_and_control.sim import pybullet_robot_interface as pb
 from simulation_and_control.controllers.servo_motor import MotorCommands
 from simulation_and_control.controllers.pin_wrapper import PinWrapper
 
-def main():
-    # Configuration for the simulation
-    package_share_directory = get_package_share_directory('mycobot_client_2')
-    conf_file_name = "elephantconfig.json"  # Configuration file for the robot
-    print(package_share_directory)
-    dir_with_configs = package_share_directory
-    sim = pb.SimInterface(conf_file_name, conf_file_path_ext = dir_with_configs)  # Initialize simulation interface
 
-    # Get active joint names from the simulation
-    ext_names = sim.getNameActiveJoints()
-    ext_names = np.expand_dims(np.array(ext_names), axis=0)  # Adjust the shape for compatibility
+COBOT_JOINT_GOAL_TOPIC = "mycobot/angles_goal"
+COBOT_POSE_GOAL_TOPIC = "mycobot/pose_goal"
+COBOT_GRIPPER_STATUS_TOPIC = "mycobot/gripper_status"
 
-    source_names = ["pybullet"]  # Define the source for dynamic modeling
+class CobotIK(Node):
+    def __init__(self):
+        super().__init__('mycobot_ik_client')
+        
+        self.cmd_angle_pub = self.create_publisher(MycobotSetAngles, COBOT_JOINT_GOAL_TOPIC, 1)
+        self.gripper_status_pub = self.create_publisher(MycobotGripperStatus, COBOT_GRIPPER_STATUS_TOPIC, 1)
 
-    # Create a dynamic model of the robot
-    dyn_model = PinWrapper(conf_file_name, "pybullet", ext_names, source_names, False,0, dir_with_configs)  
+        self.pose_sub = self.create_subscription(
+            MycobotPose,
+            COBOT_POSE_GOAL_TOPIC,
+            self.set_pose,
+            1)
+        
+        self.package_share_directory = get_package_share_directory('mycobot_client_2')
+        self.conf_file_name = "elephantconfig.json"  # Configuration file for the robot
+        
+        self.get_logger().info(f"share directory {self.package_share_directory}")
+        self.sim = pb.SimInterface(self.conf_file_name, conf_file_path_ext = self.package_share_directory)  # Initialize simulation interface
 
-    # Display dynamics information
-    print("Joint info simulator:")
-    sim.GetBotJointsInfo()
+        self.source_names = ["pybullet"]  # Define the source for dynamic modeling
+        # Get active joint names from the simulation
+        self.ext_names = self.sim.getNameActiveJoints()
+        self.ext_names = np.expand_dims(np.array(self.ext_names), axis=0)  # Adjust the shape for compatibility
 
-    print("Link info simulator:")
-    sim.GetBotDynamicsInfo()
+        # Create a dynamic model of the robot
+        self.dyn_model = PinWrapper(self.conf_file_name, "pybullet", self.ext_names, self.source_names, False, 0, self.package_share_directory)  
 
-    print("Link info pinocchio:")
-    dyn_model.getDynamicsInfo()
+        # Display dynamics information
+        self.get_logger().info("Joint info simulator:")
+        self.get_logger().info(self.sim.GetBotJointsInfo())
 
-    q_k = np.array([0, 0, 0, 0, 0, 0])
-    end_effector_frame = "joint_6"
-    local_or_global = "local_global"
-    q_k_plus_one = np.copy(q_k)
-    q_k_plus_one[:] = 99
-    target_pose = np.array([0.2, -0.1, 0, 0, 0, 0])
-    num_iterations = 0
-    while np.linalg.norm(q_k_plus_one - q_k) > 0.5:
-        q_k = np.copy(q_k_plus_one)
-        jacobian = dyn_model.ComputeJacobian(q_k, end_effector_frame, local_or_global)
-        pose_at_k = dyn_model.ComputeFK(q_k, end_effector_frame)
-        q_k_plus_one = q_k + np.linalg.inv(jacobian) @ (target_pose - pose_at_k)
-        num_iterations += 1
-    print(f"found solution in {num_iterations} iterations")
-    print(q_k_plus_one)
+        self.get_logger().info("Link info simulator:")
+        self.get_logger().info(self.sim.GetBotDynamicsInfo())
+
+        self.get_logger().info("Link info pinocchio:")
+        self.get_logger().info(self.dyn_model.getDynamicsInfo())
+
+
     
-    # use ComputeJacobian(self,q0,frame_name,local_or_global)
-    # to get jacobian, and numerical solver from allesandro's slides
+    def set_pose(self, msg):
+        q_k = np.array([msg.x, msg.y, msg.z, 0, 0, 0])
+        end_effector_frame = "joint6_flange"
+        local_or_global = "local_global"
+        q_k_plus_one = np.copy(q_k)
+        q_k_plus_one[:] = 99
+        target_pose = np.array([0.2, -0.1, 0, 0, 0, 0])
+        num_iterations = 0
+        while np.linalg.norm(q_k_plus_one - q_k) > 0.5:
+            q_k = np.copy(q_k_plus_one)
+            jacobian = self.dyn_model.ComputeJacobian(q_k, end_effector_frame, local_or_global).J
+            self.get_logger().info("jacobian")
+            self.get_logger().info(jacobian)
+            pose_at_k = self.dyn_model.ComputeFK(q_k, end_effector_frame)
+            self.get_logger().info("pose at k")
+            self.get_logger().info(pose_at_k)
+            q_k_plus_one = q_k + np.linalg.inv(jacobian) @ (target_pose - pose_at_k)
+            num_iterations += 1
+        self.get_logger().info(f"found solution in {num_iterations} iterations")
+        self.get_logger().info(q_k_plus_one)
 
-    # joint6
+        new_joint_msg = MycobotSetAngles()
+        new_joint_msg.joint_1 = q_k_plus_one[0]
+        new_joint_msg.joint_2 = q_k_plus_one[1]
+        new_joint_msg.joint_3 = q_k_plus_one[2]
+        new_joint_msg.joint_4 = q_k_plus_one[3]
+        new_joint_msg.joint_5 = q_k_plus_one[4]
+        new_joint_msg.joint_6 = q_k_plus_one[5]
+        new_joint_msg.speed = 80
 
-    # Command and control loop
-    # cmd = MotorCommands()  # Initialize command structure for motors
-    # while True:
-    #     cmd.tau_cmd = np.zeros((dyn_model.getNumberofActuatedJoints(),))  # Zero torque command
-    #     sim.Step(cmd, "torque")  # Simulation step with torque command
+        self.cmd_angle_pub.publish(new_joint_msg)
 
-    #     if dyn_model.visualizer: 
-    #         for index in range(len(sim.bot)): # Conditionally display the robot model
-    #             q = sim.GetMotorAngles(index)
-    #             dyn_model.DisplayModel(q)  # Update the display of the robot model
 
-    #     # Exit logic with 'q' key
-    #     keys = sim.GetPyBulletClient().getKeyboardEvents()
-    #     qKey = ord('q')
-    #     if qKey in keys and keys[qKey] and sim.GetPyBulletClient().KEY_WAS_TRIGGERED:
-    #         break
+def main(args=None):
+    rclpy.init(args=args)
 
-    #     time.sleep(0.1)  # Slow down the loop for better visualization
+    cobot_ik = CobotIK()
+
+    rclpy.spin(cobot_ik)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    cobot_ik.destroy_node()
+    rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
