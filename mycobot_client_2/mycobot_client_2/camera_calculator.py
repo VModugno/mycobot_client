0582b8539ac3cc38d029bf2e680b8df87b767e74
MZandtheRaspberryPi <mzandtheraspberrypi@gmail.com>
2024-10-03 20:51:03 +0100

    trying calibration

--- mycobot_client_2/mycobot_client_2/camera_calculator.py
+++ mycobot_client_2/mycobot_client_2/camera_calculator.py
@@ -17,6 +17,71 @@ import numpy as np
 import numpy.typing as npt
 import cv2
 
+
+PITCH_DEGREES = 45
+YAW_DEGREES = 45
+TRANSLATION_Z = 0.210
+TRANSLATION_Y = 0.180
+DEGREES_TO_RADIAN = 1 * np.pi / 180
+
+# cubes are 25mm a side
+GREEN_CUBE_BOTTOM_LEFT_COORDS = np.array([0.1, -0.125, 0])
+GREEN_CUBE_TOP_LEFT_COORDS = np.array([0.125, -0.125, 0])
+GREEN_CUBE_BOTTOM_LEFT_COORDS_PIXEL = np.array([607, 233])
+GREEN_CUBE_TOP_LEFT_COORDS_PIXEL = np.array([582, 218])
+
+ORANGE_CUBE_BOTTOM_LEFT_COORDS = np.array([0.175, -0.125, 0])
+ORANGE_CUBE_BOTTOM_LEFT_COORDS_PIXEL = np.array([532, 190])
+ORANGE_CUBE_TOP_LEFT_COORDS = np.array([0.175, -0.125, 0])
+ORANGE_CUBE_TOP_LEFT_COORDS_PIXEL = np.array([510, 178])
+
+RED_CUBE_BOTTOM_LEFT_COORDS = np.array([0.175, 0.1, 0])
+RED_CUBE_BOTTOM_LEFT_COORDS_PIXEL = np.array([317, 321])
+RED_CUBE_TOP_LEFT_COORDS = np.array([0.175, 0.1, 0])
+RED_CUBE_TOP_LEFT_COORDS_PIXEL = np.array([296, 301])
+
+YELLOW_CUBE_BOTTOM_LEFT_COORDS = np.array([0.075, 0.1, 0])
+YELLOW_CUBE_BOTTOM_LEFT_COORDS_PIXEL = np.array([431, 448])
+YELLOW_CUBE_BOTTOM_RIGHT_COORDS = np.array([0.175, 0.1, 0])
+YELLOW_CUBE_BOTTOM_RIGHT_COORDS_PIXEL = np.array([500, 454])
+
+
+# we have a matrix T whereby cube_world_points = T @ cube_cam_points
+CUBE_WORLD_POINTS = np.array([GREEN_CUBE_BOTTOM_LEFT_COORDS, GREEN_CUBE_TOP_LEFT_COORDS, ORANGE_CUBE_BOTTOM_LEFT_COORDS,
+                    ORANGE_CUBE_TOP_LEFT_COORDS, RED_CUBE_BOTTOM_LEFT_COORDS, RED_CUBE_TOP_LEFT_COORDS, YELLOW_CUBE_BOTTOM_LEFT_COORDS, YELLOW_CUBE_BOTTOM_RIGHT_COORDS])
+CUBE_PIXEL_COORDS = np.array([GREEN_CUBE_BOTTOM_LEFT_COORDS_PIXEL, GREEN_CUBE_TOP_LEFT_COORDS_PIXEL, ORANGE_CUBE_BOTTOM_LEFT_COORDS_PIXEL, 
+                ORANGE_CUBE_TOP_LEFT_COORDS_PIXEL, RED_CUBE_TOP_LEFT_COORDS, RED_CUBE_TOP_LEFT_COORDS_PIXEL, YELLOW_CUBE_BOTTOM_LEFT_COORDS_PIXEL, YELLOW_CUBE_BOTTOM_RIGHT_COORDS_PIXEL])
+
+# in cad, in global coordinate system X/Y/Z right handed like ros, we translated in Y, then in Z.
+# then we rotated around z by 45 degrees, then we rotated around y by 45 degrees. This point, is in the middle of the realsense
+# between the back and front (front has lenses)
+# it is on the bottom of the realsense, when camera plug is coming out the left side of realsense.
+
+# this frame has z pointing out of the camera, x pointing to the right in image plane
+# and y pointing down in image plane
+# we need to translate from this to the point we have in cad
+# we can do this by taking xyz points in this frame and setting:
+# z = -y
+# x = z
+# y = -x
+# then translating to the point in cad
+# then translating to the origin of the world
+# from the datasheet, rom Centerline of ¼-20(1) To Left Imager: D405: 9 mm
+# the centerline of the camera tripod is where our point in cad is. Left imager is depth frame. So let's take that and transform it back.
+# from that campera tripod mount to the left imager is 21mm according to the datasheet upwards. So that tells us how to translate from the cad.
+CAD_POINT_TO_CAMERA_FRAME_Z_OFFSET = 0.021
+CAD_POINT_TO_CAMERA_FRAME_Y_OFFSET = 0.009
+COLOR_CAMERA_FRAME_ID = "camera_color_optical_frame"
+DEPTH_CAMERA_FRAME_ID = "camera_depth_optical_frame"
+WORLD_FRAME_ID = "map"
+
+Y_ROTATION = np.array([[np.cos(DEGREES_TO_RADIAN * PITCH_DEGREES), 0, np.sin(DEGREES_TO_RADIAN * PITCH_DEGREES)],
+                        [0, 1, 0],
+                        [-np.sin(DEGREES_TO_RADIAN * PITCH_DEGREES), 0, np.cos(DEGREES_TO_RADIAN * PITCH_DEGREES)]])
+Z_ROTATION = np.array([[np.cos(DEGREES_TO_RADIAN * YAW_DEGREES), -np.sin(DEGREES_TO_RADIAN * YAW_DEGREES), 0],
+                        [np.sin(DEGREES_TO_RADIAN * YAW_DEGREES), np.cos(DEGREES_TO_RADIAN * YAW_DEGREES), 0],
+                        [0, 0, 1]])
+
 COLOR_CAMERA_TOPIC_NAME = "/camera/realsense2_camera_node/color/image_rect_raw"
 COLOR_CAMERA_INFO_TOPIC_NAME = "/camera/realsense2_camera_node/color/image_rect_raw/camera_info"
 DEPTH_CAMERA_TOPIC_NAME = "/camera/realsense2_camera_node/depth/image_rect_raw"
@@ -48,6 +113,7 @@ class CameraCalculator(Node):
         self.pcd_publisher = self.create_publisher(PointCloud2, POINTCLOUD_TOPIC_NAME, 1)
 
         self.color_img_frame = None
+        self.depth_img_frame = None
         self.color_img_cv = None
         self.depth_img_cv = None
         self.color_processed_intrinsics = None
@@ -78,17 +144,52 @@ class CameraCalculator(Node):
         self.color_img_cv = self.br.imgmsg_to_cv2(msg, "rgb8")
     
     def depth_img_cb(self, msg):
+        self.depth_img_frame = msg.header.frame_id
         self.depth_img_cv = self.br.imgmsg_to_cv2(msg, "16UC1")
+    
+    def translate_to_world_frame(self, xyz_rgb):
+        # go from camera optical frame to camera link frame (a typical right handed system)
+        # we can do this by taking xyz points in this frame and setting:
+        # z = -y
+        # x = z
+        # y = -x
+        x = np.copy(xyz_rgb[:, 0])
+        y = np.copy(xyz_rgb[:, 1])
+        z = np.copy(xyz_rgb[:, 2])
+        xyz_rgb[:, 0] = z
+        xyz_rgb[:, 1] = -x
+        xyz_rgb[:, 2] = -y
+        # now let's undo the pitch
+        xyz_rgb[:, 0:3] = (Y_ROTATION.T @ xyz_rgb[:, 0:3].T).T
+        # now let's undo the yaw
+        xyz_rgb[:, 0:3] = (Z_ROTATION.T @ xyz_rgb[:, 0:3].T).T
+
+        # now xyz rotation is aligned with world frame but we are translated from the camera link to the point we have in cad
+        xyz_rgb[:, 1] = xyz_rgb[:, 1]  - CAD_POINT_TO_CAMERA_FRAME_Y_OFFSET
+        xyz_rgb[:, 2] = xyz_rgb[:, 2] - CAD_POINT_TO_CAMERA_FRAME_Z_OFFSET
+
+        # now the point we know in CAD is aligned, but now we need to go back to the origin of the robot
+        xyz_rgb[:, 1] = xyz_rgb[:, 1] - TRANSLATION_Y
+        xyz_rgb[:, 2] = xyz_rgb[:, 2] - TRANSLATION_Z
+        return xyz_rgb
     
     def get_images(self):
         if self.color_img_cv is None or self.depth_img_cv is None or self.depth_processed_intrinsics is None:
-            print("none")
+            self.get_logger().error("color img was none or detph image was none or depth intrinsics was none")
             return None
         img_id = self.img_id_counter
         self.img_id_counter += 1
+        
+        # i think color camera and depth camera are aligned for this camera...
+        if self.color_img_frame != COLOR_CAMERA_FRAME_ID or self.depth_img_frame != DEPTH_CAMERA_FRAME_ID:
+            print(CAMERA_FRAME_ID)
+            print(self.depth_img_frame )
+            print(self.color_img_frame )
+            return None
 
         xyz_rgb = self.get_3d_points(self.color_img_cv, self.depth_img_cv)
-        pointcloud = self.points_to_pountcloud(xyz_rgb)
+        xyz_rgb_world_frame = self.translate_to_world_frame(xyz_rgb)
+        pointcloud = self.points_to_pountcloud(xyz_rgb_world_frame)
         self.pcd_publisher.publish(pointcloud)
         # get a color image and it's ID
         img = Images(img_id, self.color_img_cv, self.depth_img_cv, xyz_rgb, self.color_img_frame)
@@ -124,6 +225,7 @@ class CameraCalculator(Node):
         # this is how rviz2 will work it out
 
         packed_as_floats = np.zeros(pixel_colors.shape[0], dtype=np.float32)
+        pack_char = ""
 
         # there is probably a way to do this without a for loop, but i am tired
         for i in range(0, pixel_colors.shape[0]):
@@ -131,18 +233,18 @@ class CameraCalculator(Node):
             g = pixel_colors[i, 1]
             b = pixel_colors[i, 2]
             
-            r_b = struct.unpack('>I', struct.pack('>I',r))[0]
-            g_b = struct.unpack('>I', struct.pack('>I',g))[0]
-            b_b = struct.unpack('>I', struct.pack('>I',b))[0]
+            r_b = struct.unpack('I', struct.pack(pack_char + 'I',r))[0]
+            g_b = struct.unpack('I', struct.pack(pack_char + 'I',g))[0]
+            b_b = struct.unpack('I', struct.pack(pack_char + 'I',b))[0]
             b_final = (r_b << 16) | ((g_b << 8) | b_b)
-            f_final = struct.unpack('<f', struct.pack('>I',b_final))[0]
+            f_final = struct.unpack( pack_char + 'f', struct.pack('I',b_final))[0]
             packed_as_floats[i] = f_final
         self.get_logger().debug("checking bit  calcs")
-        self.get_logger().info(pixel_colors[0])
-        self.get_logger().info(np.unpackbits(pixel_colors[0]))
-        self.get_logger().info(np.unpackbits(packed_as_floats[0:1].view(np.uint8)))
+        self.get_logger().debug(np.array_str(pixel_colors[0]))
+        self.get_logger().debug(np.array_str(np.unpackbits(pixel_colors[0])))
+        self.get_logger().debug(np.array_str(np.unpackbits(packed_as_floats[0:1].view(np.uint8))))
 
-        return np.concatenate((x[:, None], y[:, None], z[:, None], rgb_float[: None]), axis=1, dtype=np.float32)
+        return np.concatenate((x[:, None], y[:, None], z[:, None], packed_as_floats[:, None]), axis=1, dtype=np.float32)
 
     def points_to_pountcloud(self, xyz_rgb):
 
@@ -151,7 +253,7 @@ class CameraCalculator(Node):
 
         # The PointCloud2 message also has a header which specifies which 
         # coordinate frame it is represented in. 
-        header =  Header(frame_id=self.color_img_frame)
+        header =  Header(frame_id=WORLD_FRAME_ID)
         header.stamp = self.get_clock().now().to_msg()
 
         
@@ -169,6 +271,22 @@ class CameraCalculator(Node):
             row_step=4 * xyz_rgb.shape[0],
             data=xyz_rgb.tobytes()
         )
+    
+    def get_3d_points_from_pixel_point_on_color(self, img: Images, u, v):
+
+        z = DEPTH_SCALE * img.depth[v, u]
+
+        fx = self.depth_processed_intrinsics[0, 0]
+        fy = self.depth_processed_intrinsics[1, 1]
+        cx = self.depth_processed_intrinsics[0, 2]
+        cy = self.depth_processed_intrinsics[1, 2]
+
+        x = (u - cx) * z / fx
+        y = (v - cy) * z / fy
+        rgb = 1.0 # dummy float
+        point_in_cam_frame = np.array([[x, y, z, rgb]])
+        point_in_world_frame = self.translate_to_world_frame(point_in_cam_frame)
+        return point_in_cam_frame[:, 0:3].flatten(), point_in_world_frame[:, 0:3].flatten()
 
 
     def display_img(self, img: Images):
@@ -192,11 +310,14 @@ def main(args=None):
     rate = camera_calculator.create_rate(30)
 
     while rclpy.ok():
-        print("getting image")
         img = camera_calculator.get_images()
         if img is None:
+            camera_calculator.get_logger().error("frame was None")
             continue
-        # camera_calculator.display_img(img)
+        cam_points, world_points = camera_calculator.get_3d_points_from_pixel_point_on_color(img, 332, 292)
+        camera_calculator.get_logger().info("cam frame: " + np.array_str(cam_points))
+        camera_calculator.get_logger().info("world frame: " + np.array_str(world_points))
+        camera_calculator.display_img(img)
         rate.sleep()
         
     
